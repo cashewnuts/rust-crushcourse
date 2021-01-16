8f8629ac9365c64c6c89283d36ce25dd61089d09
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2021-01-17 08:24:00 +0900

    Completed types.

--- types/src/main.rs
+++ types/src/main.rs
@@ -1,3 +1,17 @@
+extern crate unicode_segmentation;
+
+
+use std::string::String;
+use unicode_segmentation::UnicodeSegmentation;
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world!");
+
+    s.push_str(" world");
+
+    println!("{}", s);
+    match s.grapheme_indices(true).nth(3) {
+        Some((nsize, c)) => println!("{} {}", c, nsize),
+        None => println!("NONE"),
+    }
 }
